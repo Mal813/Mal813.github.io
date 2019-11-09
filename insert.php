0e7c9e22c9d3ee3712e9eb1eb86a8ac0afd6f8a3
Mal813 <57512576+Mal813@users.noreply.github.com>
2019-11-09 01:36:57 +0100

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_POST['submit'])){
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$nick = $_POST['nick'];
+$country = $_POST['country'];
+$dob = $_POST['dob'];
+$mail = $_POST['mail'];
+
+$dbconnect=mysqli_connect('localhost','root','','signup');
+
+$sql = mysqli_query($dbconnect, "insert into form(id_form,first_name,last_name,nickname,country,birth_date,email) values('','$firstname','$lastname','$nick','$country','$dob','$mail')");
+if ($sql) {
+    echo"Inserted";
+    }
+    else {
+        echo "Error";
+}
+}
+?>
